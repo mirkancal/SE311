454331754ff4a15367a1bf39e5eca1efa34dca47
mirkancal <mirkan.caliskan@std.ieu.edu.tr>
2018-03-17 13:30:35 +0300

    Class hierarchy added to homework file

---
+++ homework1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class ExpressionTree{};
+class ComponentNode : public ExpressionTree {};
+class LeafNode : public ComponentNode {};
+class CompositeBinaryNode : public ComponentNode{};
+class CompositeAddNode : public CompositeBinaryNode{};
+class CompositeSubstractNode : public CompositeBinaryNode{};
+class CompositeMultiplyNode : public CompositeBinaryNode{};
+class CompositeDivideNode : public CompositeBinaryNode{};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
